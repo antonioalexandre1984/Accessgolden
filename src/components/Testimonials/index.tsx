c23efad7656504412839fa8d393964192fd8b481
antonioalexandre1984 <antonioalexandre1984@gmail.com>
2023-03-06 16:50:14 -0300

    alter price

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -5,12 +5,12 @@ export function Testimonials() {
   const { icon } = community;
   return (
     <section id='testimonials'>
-      <div className="container mx-auto px-4 md:px-0 mt-24 py-8 md:py-0">
+      <div className="container mx-auto px-4 md:px-0 mt-48 py-8 md:py-0">
         <div
           /* data-aos='fade-up'
           data-aos-delay='400'
           data-aos-offset='400' */
-          className="section-title-group max-w-[740px] mx-auto md:px-0 mt-12 md:mt-0">
+          className="section-title-group max-w-[740px] mx-auto md:px-0 mt-48 md:mt-0">
           <img src={icon} alt="" className="" />
           <h2
             style={{ textShadow: "0 0 10px rgba(0,0,0,0.5)" }}
